f3358470ff6975e5d8403328240c7e674f76b519
Sergio Guzman <serguz95444@gmail.com>
2025-01-16 16:16:13 -0600

    Project complete: Initial version is fully functional.

---
+++ unidad1/app/Program.cs
@@ -0,0 +1,18 @@
+﻿// Prices of the products
+const  float monitorPrice = 4500.00F;
+const  float keyboardPrice = 1800.00F;
+const  float mousePrice = 1200.00F;
+const  float chairPrice = 5000.00F;
+
+
+// Discount rate (12%)
+float discountRate = 0.12F;
+
+// Calculate total price and total discount
+float totalPrice = monitorPrice + keyboardPrice + mousePrice + chairPrice;
+float totalDiscount = totalPrice * discountRate;
+float finalPrice = totalPrice - totalDiscount;
+
+Console.WriteLine($"Total price: ${totalPrice}");
+Console.WriteLine($"Total discount (12%): ${totalDiscount}");
+Console.WriteLine($"Final price after dicount: ${finalPrice}");
