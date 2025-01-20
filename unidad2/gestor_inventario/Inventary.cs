4409cf7011691ef078814fe3810d967c9b994a99
Sergio Guzman <serguz95444@gmail.com>
2025-01-20 14:51:53 -0600

    the main class for inventory management has been added.

--- unidad2/gestor_inventario/Inventary.cs
+++ unidad2/gestor_inventario/Inventary.cs
@@ -5,15 +5,81 @@ public class Inventory
 {
     public List<Product> productList = new List<Product>();
 
-    public void AddProduct(Product newProduct)
+    public void AddProduct()
     {
+		Console.Write("Enter the product name: ");
+        string? name = Console.ReadLine();
+        if (string.IsNullOrEmpty(name))
+    	{
+        	Console.WriteLine("Product name cannot be empty or null. Operation canceled.");
+        	return;
+    	}
+
+        Console.Write("Enter the product price: ");
+        if (!decimal.TryParse(Console.ReadLine(), out decimal price))
+        {
+            Console.WriteLine("Price must be a valid number. Operation canceled.");
+            return;
+        }
+
+        Console.Write("Enter the product quantity: ");
+        if (!int.TryParse(Console.ReadLine(), out int quantity))
+        {
+            Console.WriteLine("Quantity must be a valid integer. Operation canceled.");
+            return;
+        }
+
+        var newProduct = new Product(name, price, quantity);
         productList.Add(newProduct);
         Console.WriteLine($"Product {newProduct.Name} added to the inventory.");
     }
 
-    public void RemoveProduct(string name)
+	public void ModifyProduct()
+	{
+		Console.Write("Enter the product name: ");
+        string? name = Console.ReadLine();
+        if (string.IsNullOrEmpty(name))
+    	{
+        	Console.WriteLine("Product name cannot be empty or null. Operation canceled.");
+        	return;
+    	}
+
+		Product? productModify = productList.Find(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+		if (productModify != null)
+		{
+			Console.Write("Enter the product price: ");
+        	if (!decimal.TryParse(Console.ReadLine(), out decimal newPrice) || newPrice < 0)
+        	{
+            	Console.WriteLine("Price must be a valid number. Operation canceled.");
+            	return;
+        	}
+
+			Console.Write("Enter the product quantity: ");
+			if (!int.TryParse(Console.ReadLine(), out int newQuantity) || newQuantity < 0)
+			{
+				Console.WriteLine("Quantity must be a valid integer. Operation canceled.");
+				return;
+			}
+
+			productModify.Price = newPrice;
+			productModify.Quantity = newQuantity;
+			
+			Console.WriteLine($"Product {productModify.Name} successfully modified");
+		}
+	}
+
+    public void RemoveProduct()
     {
-        Product productToRemove = productList.Find(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+		Console.Write("Enter the name of the product to remove: ");
+    	string? name = Console.ReadLine();
+
+    	if (string.IsNullOrWhiteSpace(name))
+    	{
+       		Console.WriteLine("Product name cannot be empty or null. Operation canceled.");
+       		return;
+    	}
+        Product? productToRemove = productList.Find(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
 
         if (productToRemove != null)
         {
@@ -23,9 +89,9 @@ public class Inventory
         else
         {
             Console.WriteLine($"Product {name} not found.");
+			return;
         }
     }
-
 
     public void ShowInventory()
     {
@@ -38,7 +104,8 @@ public class Inventory
         Console.WriteLine("\n--- Inventory ---");
         foreach (Product product in productList)
         {
-            Console.WriteLine($"{product.Name} - {product.Price:C} x {product.Quantity} units");
+            Console.WriteLine($"{product.Name} - {product.Price:C} x {product.Quantity} units | Total value of the product:${product.Price * product.Quantity}");
+			Console.WriteLine($"Total value of inventory: ${CalculateTotalValue()}");
         }
     }
 
