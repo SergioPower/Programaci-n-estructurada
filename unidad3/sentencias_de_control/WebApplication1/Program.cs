222b7a1eeed9ce59fde804f6ba09381b4fae2c81
Sergio Guzman <serguz95444@gmail.com>
2025-01-29 13:50:37 -0600

    Refactor programa de suma de números: se agrega menú de opciones y validación de entrada

--- unidad3/sentencias_de_control/WebApplication1/Program.cs
+++ unidad3/sentencias_de_control/WebApplication1/Program.cs
@@ -1,59 +1,64 @@
 using System;
 
-class SumaDeNumeros
+class Program
 {
 	static void Main(string[] args)
 	{
-		// Array para almacenas los números
-		int[] numbers = new int[3];
-
-		Console.WriteLine("¡Bienvenido!");
-
-		// Solicita datos por teclado
-		for (int i = 0; i < numbers.Length; i++)
+		Console.WriteLine("Bienvenido");
+		while (true)
 		{
-			// Valida entrada de números
-			bool isValid = false;
-			while (!isValid)
+			Console.WriteLine("Ingresa tres números:");
+
+			int a = ReadNumber("Número a: ");
+			int b = ReadNumber("Número b: ");
+			int c = ReadNumber("Número c: ");
+
+			Console.WriteLine("Elija una opción:");
+			Console.WriteLine("1. Validar suma");
+			Console.WriteLine("2. Salir");
+
+			int option = ReadNumber(" ");
+
+			switch (option)
 			{
-				Console.WriteLine($"Ingrese número {i + 1}: ");
-				string? input = Console.ReadLine();
+				case 1:
+					if (a + b == c || b + c == a || a + c == b)
+					{
+						Console.WriteLine("SON IGUALES");
+					}
+					else
+					{
+						Console.WriteLine("SON DISTINTOS");
+					}
 
-				if (int.TryParse(input, out numbers[i]))
-				{
-					isValid = true; // Si la conversión es exitosa, salimos del bucle
-				}
-				else
-				{
-					Console.WriteLine("Por favor, ingresa un número válido.");
-				}
+					Console.WriteLine("------------------------------");
+					break;
+
+				case 2:
+					Console.WriteLine("Saliendo del programa...");
+					return;
+				default:
+					Console.WriteLine("Opción no válida");
+					break;
 			}
+
 		}
-
-		// Menú
-		Console.WriteLine("Selecciona una opción:");
-		Console.WriteLine("1. Validar Suma  2. Salir");
-		string? option = Console.ReadLine();
-
-		switch (option)
-		{
-			case "1":
-				Suma();
-				break;
-
-			case "2":
-				Console.WriteLine("Salir");
-				break;
-			default:
-				Console.WriteLine("Opción invalida");
-				break;
-		}
-
-
 	}
 
-	static void Suma()
+	static int ReadNumber(string message)
 	{
-		Console.WriteLine("Suma");
+		int number;
+
+		while (true)
+		{
+			Console.Write(message);
+			if (int.TryParse(Console.ReadLine(), out number))
+			{
+				return number;
+			}
+
+			Console.WriteLine("Entrada invalida. Intente de nuevo.");
+
+		}
 	}
 }
--- unidad3/sentencias_de_control/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ unidad3/sentencias_de_control/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8281935954a8e2ff995eb6d66fad072ecf857078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ee01c7f3933d674e977ba9f84a6841fc12610d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
